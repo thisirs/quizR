a85a48c3c1b461317b3cce50b5b4b28d0188d0db
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2016-10-16 22:34:11 +0200

    Add cloze.R

---
+++ R/cloze.R
@@ -0,0 +1,69 @@
+#' Number of fields in cloze question
+#'
+#' @param text Some character string
+get_cloze_num <- function(text) {
+    stringi::stri_count_regex(text, cloze_regex)
+}
+
+#' Return coefficients of cloze fields over their sum
+#'
+#' @param question A question
+cloze_coefficients <- function(question) {
+    stopifnot(question$type == "cloze")
+    coeffs <- as.numeric(stringi::stri_match_all_regex(question$text, cloze_regex)[[1]][,2])
+    coeffs / sum(coeffs)
+}
+
+split_cloze_guesses <- function(num, s_answers) {
+    prefix <- "partie (\\d+) : "
+    numbers <- as.numeric(stringi::stri_match_all_regex(s_answers, prefix)[[1]][,2])
+    stopifnot(numbers == (1:num))
+
+    raw_answers <- stringi::stri_split_regex(s_answers, prefix, omit_empty=TRUE)[[1]]
+    stopifnot(length(raw_answers) == num)
+    answers0 <- trimws(raw_answers)
+
+    ## Remove trailing semicolon
+    sub(";$", "", answers0)
+}
+
+
+correct_question_cloze <- function(question, env, guess) {
+    stopifnot(is.list(question$answer))
+    num <- get_cloze_num(question$text)
+    stopifnot(length(question$answer) == num)
+    guesses <- split_cloze_guesses(num, guess)
+    stopifnot(length(guesses) == num)
+
+    cloze_points <- rep(0, num)
+    right_answers <- vector(mode="list", length=num)
+
+    for (i in 1:num) {
+        guess <- guesses[i]
+        answer_raw <- question$answer[[i]]
+
+        # Possibly several right answers, listify them
+        answers <- if(is.list(answer_raw)) answer_raw else list(answer_raw)
+        ea <- eval_answers(answers, env)
+        match <- match_answers(ea, guess, question$dist, question$epsilon)
+
+        if(any(match)) {
+            cloze_points[i] <- 1
+            right_answers[[i]] <- answers[match][[1]]
+        } else {
+            cloze_points[i] <- 0
+            right_answers[[i]] <- answers[[1]]
+        }
+    }
+
+    cloze_coefficients <- cloze_coefficients(question)
+    weighted_points <- cloze_points * cloze_coefficients
+    total_points <- question$points * sum(weighted_points)
+
+    list(type="cloze",
+         points=total_points,
+         cloze.points=cloze_points,
+         cloze.coeffs=cloze_coefficients,
+         guesses=guesses,
+         right_answers=right_answers)
+}
--- R/quizR.R
+++ R/quizR.R
@@ -178,13 +178,6 @@ get_num <- function(group) {
     }
 }
 
-#' Number of fields in cloze question
-#'
-#' @param text Some character string
-get_cloze_num <- function(text) {
-    stringi::stri_count_regex(text, cloze_regex)
-}
-
 questionTypes <- c("shortanswer", "description", "cloze")
 
 #' Create a question object
@@ -287,54 +280,9 @@ match_answers <- function(eval_answers, guess, dist, epsilon) {
     })
 }
 
-#' Return coefficients of cloze fields over their sum
-#'
-#' @param question A question
-cloze_coefficients <- function(question) {
-    stopifnot(question$type == "cloze")
-    coeffs <- as.numeric(stringi::stri_match_all_regex(question$text, cloze_regex)[[1]][,2])
-    coeffs / sum(coeffs)
-}
-
 correct_question <- function(question, env, guess) {
     if(question$type == "cloze") {
-        stopifnot(is.list(question$answer))
-        num <- get_cloze_num(question$text)
-        stopifnot(length(question$answer) == num)
-        guesses <- split_cloze_guesses(num, guess)
-        stopifnot(length(guesses) == num)
-
-        cloze_points <- rep(0, num)
-        right_answers <- vector(mode="list", length=num)
-
-        for (i in 1:num) {
-            guess <- guesses[i]
-            answer_raw <- question$answer[[i]]
-
-            # Possibly several right answers, listify them
-            answers <- if(is.list(answer_raw)) answer_raw else list(answer_raw)
-            ea <- eval_answers(answers, env)
-            match <- match_answers(ea, guess, question$dist, question$epsilon)
-
-            if(any(match)) {
-                cloze_points[i] <- 1
-                right_answers[[i]] <- answers[match][[1]]
-            } else {
-                cloze_points[i] <- 0
-                right_answers[[i]] <- answers[[1]]
-            }
-        }
-
-        cloze_coefficients <- cloze_coefficients(question)
-        weighted_points <- cloze_points * cloze_coefficients
-        total_points <- question$points * sum(weighted_points)
-
-        list(type="cloze",
-             points=total_points,
-             cloze.points=cloze_points,
-             cloze.coeffs=cloze_coefficients,
-             guesses=guesses,
-             right_answers=right_answers)
+        correct_question_cloze(question, env, guess)
     } else {
         # Possibly several right answers, listify them
         answers <- if(is.list(question$answer)) question$answer else list(question$answer)
@@ -621,17 +569,4 @@ correct_record_group <- function(group, record, env, isWithQuestionBody) {
         resultg$questions[[length(resultg$questions) + 1]] <- result
     }
     return(resultg)
-}
-
-split_cloze_guesses <- function(num, s_answers) {
-    prefix <- "partie (\\d+) : "
-    numbers <- as.numeric(stringi::stri_match_all_regex(s_answers, prefix)[[1]][,2])
-    stopifnot(numbers == (1:num))
-
-    raw_answers <- stringi::stri_split_regex(s_answers, prefix, omit_empty=TRUE)[[1]]
-    stopifnot(length(raw_answers) == num)
-    answers0 <- trimws(raw_answers)
-
-    ## Remove trailing semicolon
-    sub(";$", "", answers0)
 }
