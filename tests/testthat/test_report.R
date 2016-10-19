3a810fcff30a009686a057641d247ec4aa7260ab
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2016-10-19 13:55:55 +0200

    Fix cloze tests

--- tests/testthat/test_report.R
+++ tests/testthat/test_report.R
@@ -4,44 +4,40 @@ test_that("automatic_feedback return correct markdown", {
     question <- Question("Q1", type="shortanswer", answer=quote(1))
     unrandomize_data(question)
     env <- new.env(parent=.GlobalEnv)
-    expect_identical(automatic_feedback(3, question, env), trimws("
-```{r include=FALSE}
+    expect_identical(automatic_feedback(3, question, env), "```{r include=FALSE}
 {
 }
 ```
-**Question 3.** Q1
-
-**Réponse:**
 ```{r include=FALSE}
 answer <- {1}
 ```
+**Question 3.** Q1
+
+**Réponse:** $`r answer`$
 ```{r}
 1
 ```
 
-La réponse est: $`r answer`$
-"))})
+")})
 
 test_that("automatic_feedback return correct markdown", {
     question <- Question("Q1", type="shortanswer", hidden.data=quote({a <- 1}), answer=quote(1))
     unrandomize_data(question)
     env <- new.env(parent=.GlobalEnv)
-    expect_identical(automatic_feedback(3, question, env), trimws("
-```{r include=FALSE}
+    expect_identical(automatic_feedback(3, question, env), "```{r include=FALSE}
 a <- 1
 ```
-**Question 3.** Q1
-
-**Réponse:**
 ```{r include=FALSE}
 answer <- {1}
 ```
+**Question 3.** Q1
+
+**Réponse:** $`r answer`$
 ```{r}
 1
 ```
 
-La réponse est: $`r answer`$
-"))})
+")})
 
 test_that("answer_cloze_feedback return correct markdown", {
     question <- Question("blah {1:SA:=}", type="cloze", hidden.data=quote({a <- 1}), answer=quote(1))
@@ -57,11 +53,11 @@ a <- 1
 ```{r include=FALSE}
 answer <- {1}
 ```
-1. ```{r}
+1. La réponse est: $`r answer`$
+```{r}
 1
 ```
 
-La réponse est: $`r answer`$
 ")})
 
 test_that("answer_cloze_feedback return correct markdown", {
@@ -78,40 +74,38 @@ a <- 1
 ```{r include=FALSE}
 answer <- {1}
 ```
-1. ```{r}
+1. La réponse est: $`r answer`$
+```{r}
 1
 ```
 
-La réponse est: $`r answer`$
 ```{r include=FALSE}
 answer <- {2}
 ```
-2. ```{r}
+2. La réponse est: $`r answer`$
+```{r}
 2
 ```
 
-La réponse est: $`r answer`$
 ")})
 
 test_that("general_feedback with alt.answer", {
     question <- Question("Q1", type="shortanswer", answer=quote(1))
     unrandomize_data(question)
     env <- new.env(parent=.GlobalEnv)
-    expect_identical((general_feedback(alt.answer="blah"))(3, question, env), trimws("
-```{r include=FALSE}
+    expect_identical((general_feedback(alt.answer="blah"))(3, question, env), "```{r include=FALSE}
 {
 }
 ```
-**Question 3.** Q1
-
-**Réponse:**
 ```{r include=FALSE}
 answer <- {1}
 ```
+**Question 3.** Q1
+
+**Réponse:** $`r answer`$
 blah
 
-La réponse est: $`r answer`$
-"))})
+")})
 
 
 test_that("answerstr correctly returns a string from a language", {
