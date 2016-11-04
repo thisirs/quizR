1e7bf9216e1996713ca6f22703f121a149e66549
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2016-11-04 17:12:03 +0100

    Newline when reporting error

--- R/quizR.R
+++ R/quizR.R
@@ -444,7 +444,7 @@ distinct_data <- function(quiz, lang) {
         }
     }
     errors <- sapply(languages, fails)
-    if (any(errors)) stop("Some errors in data code chunks", languages[errors])
+    if (any(errors)) stop("Some errors in data code chunks:\n", languages[errors])
     length(languages) < 2 || all(utils::combn(languages, 2, function(args) do.call(distinct_language, args, quote = TRUE)))
 }
 
