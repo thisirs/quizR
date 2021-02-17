d535a72b3cba72a2aa1296c872a294aa5247fb8d
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2021-02-17 14:56:24 +0100

    Use items in tests

--- tests/testthat/test_multiple_choice.R
+++ tests/testthat/test_multiple_choice.R
@@ -1,12 +1,12 @@
 context("Testing multiple choice questions")
 
 test_that(paste(sQuote("get_inst_cookie"), "is working correctly"), {
-    q1 <- Question("q1", type = "mc", statements = c("stat 1", "stat 2"), answer = c(T, F))
+    q1 <- Question("q1", type = "mc", items = c("stat 1", "stat 2"), answer = c(T, F))
     expect_equal(q1$get_inst_cookie(list(), list(data = quote({}), env = new.env())), "{1:MC:=stat 1~stat 2}")
 
-    q1 <- Question("q1", type = "mc", statements = c("stat 1", "stat 2"), answer = c(F, T))
+    q1 <- Question("q1", type = "mc", items = c("stat 1", "stat 2"), answer = c(F, T))
     expect_equal(q1$get_inst_cookie(list(), list(data = quote({}), env = new.env())), "{1:MC:stat 1~=stat 2}")
 
-    q1 <- Question("q1", type = "mc", statements = c("stat 1", "stat 2", "stat 3"), answer = c(F, T, T))
+    q1 <- Question("q1", type = "mc", items = c("stat 1", "stat 2", "stat 3"), answer = c(F, T, T))
     expect_equal(q1$get_inst_cookie(list(), list(data = quote({}), env = new.env())), "{1:MC:stat 1~=stat 2~=stat 3}")
 })
