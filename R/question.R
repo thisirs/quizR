1cec68a6746e8b2030510033fb24f861c023265b
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2021-03-04 15:05:36 +0100

    Add classes

--- R/question.R
+++ R/question.R
@@ -6,7 +6,25 @@ Question_classes <- list(
     "sa" = ShortanswerQuestion,
     "multiplechoice" = MultipleChoice,
     "multichoice" = MultipleChoice,
-    "mc" = MultipleChoice
+    "mc" = MultipleChoice,
+    "multichoice_v" = MultipleChoice,
+    "mcv" = MultipleChoice,
+    "multichoice_h" = MultipleChoice,
+    "mch" = MultipleChoice,
+    "multiresponse" = MultipleChoice,
+    "mr" = MultipleChoice,
+    "multiresponse_h" = MultipleChoice,
+    "mrh" = MultipleChoice,
+    "multichoice_s" = MultipleChoice,
+    "mcs" = MultipleChoice,
+    "multichoice_vs" = MultipleChoice,
+    "mcvs" = MultipleChoice,
+    "multichoice_hs" = MultipleChoice,
+    "mchs" = MultipleChoice,
+    "multiresponse_s" = MultipleChoice,
+    "mrs" = MultipleChoice,
+    "multiresponse_hs" = MultipleChoice,
+    "mrhs" = MultipleChoice
 )
 
 #' Factory class that creates questions object give their type
@@ -16,10 +34,14 @@ Question <- function(...) {
     if (is.null(args$type))
         NumericalQuestion$new(...)
     else {
-        name <- tolower(gsub("[_-]", " ", args$type))
+        name <- tolower(args$type)
         class <- Question_classes[[name]]
         if (is.null(class))
             stop("Unknown question type: ", sQuote(args$type))
+
+        if(identical(class, MultipleChoice))
+            args$format <- name
+
         args$type <- NULL
         do.call(class$new, args, quote = TRUE)
     }
